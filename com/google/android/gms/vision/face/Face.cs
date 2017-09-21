3090a618b9c6e01b5ae78348b58ac74f0bc3eab6
EQ_htatsuguchi <tatsuguchi@eq-inc.jp>
2017-09-21 09:38:54 +0900

    Face / Landmarkの実装を追加

--- com/google/android/gms/vision/face/Face.cs
+++ com/google/android/gms/vision/face/Face.cs
@@ -1,12 +1,79 @@
-﻿using UnityEngine;
+﻿using android.graphics;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace com.google.android.gms.vision.face
 {
     public class Face : BaseAndroidJavaObjectWrapper
     {
+        public static readonly float UNCOMPUTED_PROBABILITY = new AndroidJavaClass("com.google.android.gms.vision.face.Face").GetStatic<float>("UNCOMPUTED_PROBABILITY");
+
         public Face(AndroidJavaObject androidJO) : base(androidJO)
         {
             // 処理なし
+        }
+
+        public float GetEulerY()
+        {
+            return mAndroidJO.Call<float>("getEulerY");
+        }
+
+        public float GetEulerZ()
+        {
+            return mAndroidJO.Call<float>("getEulerZ");
+        }
+
+        public float GetHeight()
+        {
+            return mAndroidJO.Call<float>("getHeight");
+        }
+
+        public int GetId()
+        {
+            return mAndroidJO.Call<int>("getId");
+        }
+
+        public float GetIsLeftEyeOpenProbability()
+        {
+            return mAndroidJO.Call<float>("getIsLeftEyeOpenProbability");
+        }
+
+        public float GetIsRightEyeOpenProbability()
+        {
+            return mAndroidJO.Call<float>("getIsRightEyeOpenProbability");
+        }
+
+        public float GetIsSmilingProbability()
+        {
+            return mAndroidJO.Call<float>("getIsSmilingProbability");
+        }
+
+        public List<Landmark> GetLandmarks()
+        {
+            AndroidJavaObject landmarkListJO = mAndroidJO.Call<AndroidJavaObject>("getLandmarks");
+            List<Landmark> ret = new List<Landmark>();
+
+            if (landmarkListJO != null)
+            {
+                int size = landmarkListJO.Call<int>("size");
+
+                for (int i = 0; i < size; i++)
+                {
+                    ret.Add(new Landmark(landmarkListJO.Call<AndroidJavaObject>("get")));
+                }
+            }
+
+            return ret;
+        }
+
+        public PointF GetPosition()
+        {
+            return new PointF(mAndroidJO.Call<AndroidJavaObject>("getPosition"));
+        }
+
+        public float GetWidth()
+        {
+            return mAndroidJO.Call<float>("getWidth");
         }
     }
 }
---
+++ com/google/android/gms/vision/face/Landmark.cs
@@ -0,0 +1,37 @@
+﻿using android.graphics;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.google.android.gms.vision.face
+{
+    public class Landmark : BaseAndroidJavaObjectWrapper
+    {
+        public static readonly int BOTTOM_MOUTH = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("BOTTOM_MOUTH");
+        public static readonly int LEFT_CHEEK = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("LEFT_CHEEK");
+        public static readonly int LEFT_EAR = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("LEFT_EAR");
+        public static readonly int LEFT_EAR_TIP = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("LEFT_EAR_TIP");
+        public static readonly int LEFT_EYE = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("LEFT_EYE");
+        public static readonly int LEFT_MOUTH = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("LEFT_MOUTH");
+        public static readonly int NOSE_BASE = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("NOSE_BASE");
+        public static readonly int RIGHT_CHEEK = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("RIGHT_CHEEK");
+        public static readonly int RIGHT_EAR = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("RIGHT_EAR");
+        public static readonly int RIGHT_EAR_TIP = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("RIGHT_EAR_TIP");
+        public static readonly int RIGHT_EYE = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("RIGHT_EYE");
+        public static readonly int RIGHT_MOUTH = new AndroidJavaClass("com.google.android.gms.vision.face.Landmark").GetStatic<int>("RIGHT_MOUTH");
+
+        public Landmark(AndroidJavaObject androidJO) : base(androidJO)
+        {
+            // 処理なし
+        }
+
+        public PointF GetPosition()
+        {
+            return new PointF(mAndroidJO.Call<AndroidJavaObject>("getPosition"));
+        }
+
+        public int GetTypeForJava()
+        {
+            return mAndroidJO.Call<int>("getType");
+        }
+    }
+}
